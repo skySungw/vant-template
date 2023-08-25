e68ea3dec0e7d521178c119568e8321ec4318499
zhangyang <zhangyang@gmail.com>
2023-08-25 14:06:56 +0800

    update: 加路由限制

--- src/router/index.js
+++ src/router/index.js
@@ -2,14 +2,32 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import IndexPage from '../views/index';
 
-Vue.use(Router)
+Vue.use(Router);
 
-export default new Router({
+const router = new Router({
   routes: [
     {
       path: '/',
       name: 'Home',
+      meta: {
+        title: '首页',
+      },
       component: IndexPage
     }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  document.title = to.meta.title || '';
+  next();
+  // // 拦截校验
+  // if (!user && to.path !== '/login') {
+  //     next('/login');
+  // } else if (to.meta.permission) {
+  //     user.permission === to.meta.permission ? next() : message.alert('没有权限');
+  // } else {
+  //     next();
+  // }
+})
+
+export default router;
